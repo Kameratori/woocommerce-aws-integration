585876810d0da90c0e7b0e690fa020f392e6ddc6
Viljami Kuosmanen <viljami.kuosmanen@futurice.com>
2020-05-04 08:47:53 +0200

    Release 1.2.5

--- woocommerce-aws-integration.php
+++ woocommerce-aws-integration.php
@@ -3,7 +3,7 @@
  * Plugin name: AWS Integration for WooCommerce
  * Plugin URI: https://github.com/Toriverkosto/woocommerce-aws-integration
  * Description: WooCommerce extension to publish events to AWS services from WooCommerce hooks
- * Version: 1.2.4
+ * Version: 1.2.5
  * Author: Toriverkosto
  * Author URI: https://github.com/Toriverkosto
  * License: GPLv3
